9396aeb90e5fc9c06a2b8caec745fe1d2aa5285d
San <email>
2018-08-01 17:23:14 +0300

    a little bit of logging

--- src/main/java/xyz/hardliner/align/service/WebServiceController.java
+++ src/main/java/xyz/hardliner/align/service/WebServiceController.java
@@ -31,7 +31,9 @@ public class WebServiceController {
 	                             @RequestParam(value = "brand", required = false) String brand,
 	                             @RequestParam(value = "price") Double price,
 	                             @RequestParam(value = "quantity") Integer quantity) {
-		return handler.save(name, brand, price, quantity);
+		Product product = handler.save(name, brand, price, quantity);
+		log.info("Incoming POST request on '/product'\nNew product successfully created: " + product.toString());
+		return product;
 	}
 
 	@PutMapping("/product")
@@ -40,25 +42,35 @@ public class WebServiceController {
 	                             @RequestParam(value = "brand", required = false) String brand,
 	                             @RequestParam(value = "price", required = false) Double price,
 	                             @RequestParam(value = "quantity", required = false) Integer quantity) {
-		return handler.save(id, name, brand, price, quantity);
+		Product product = handler.save(id, name, brand, price, quantity);
+		log.info("Incoming PUT request on '/product'\nProduct successfully updated: " + product.toString());
+		return product;
 	}
 
 	@Transactional
 	@DeleteMapping("/product")
 	public void removeProduct(@RequestParam(value = "id") Long id) {
 		handler.removeById(id);
+		log.info("Incoming DELETE request on '/product'\nProduct removed by Id=" + id);
 	}
 
 	@RequestMapping(value = "/products")
 	public List<Product> getProducts(@RequestParam(value = "name", required = false) String name,
 	                                 @RequestParam(value = "brand", required = false) String brand) {
-		return handler.find(name, brand);
+		List<Product> products = handler.find(name, brand);
+		log.info("Incoming GET request on '/products'\n" + products.size() + " products found by params {"
+				+ (name == null ? "" : "name='" + name + "';")
+				+ (brand == null ? "" : "brand='" + name + "'") + "}");
+		return products;
 	}
 
 	@RequestMapping(value = "/products/xls")
 	public ResponseEntity<byte[]> getProductsXls(@RequestParam(value = "name", required = false) String name,
 	                                             @RequestParam(value = "brand", required = false) String brand) {
 		List<Product> products = handler.find(name, brand);
+		log.info("Incoming GET request on '/products'\n" + products.size() + " products found by params {"
+				+ (name == null ? "" : "name='" + name + "';")
+				+ (brand == null ? "" : "brand='" + name + "'") + "}, exporting XLS file");
 		return ResponseEntity.ok()
 				.header(CONTENT_DISPOSITION, "attachment; filename=\"" + XlsGenerator.fileName() + "\"")
 				.body(xlsGenerator.create(products));
@@ -66,7 +78,9 @@ public class WebServiceController {
 
 	@RequestMapping("/leftovers")
 	public List<Product> getLeftovers() {
-		return handler.getLeftovers();
+		List<Product> products = handler.getLeftovers();
+		log.info("Incoming GET request on '/leftovers'\n" + products.size() + " products found as leftovers");
+		return products;
 	}
 
 	@ExceptionHandler(IllegalArgumentException.class)
